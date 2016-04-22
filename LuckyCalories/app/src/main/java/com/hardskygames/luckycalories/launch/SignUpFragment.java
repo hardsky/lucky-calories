8653cac89aa7db8db46661a72dac27e5f237773a
hardsky <hardsky@yandex.ru>
2016-04-22 17:10:41 +0300

    minor changes

--- LuckyCalories/app/src/main/java/com/hardskygames/luckycalories/launch/SignUpFragment.java
+++ LuckyCalories/app/src/main/java/com/hardskygames/luckycalories/launch/SignUpFragment.java
@@ -2,6 +2,7 @@ package com.hardskygames.luckycalories.launch;
 
 
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.AppCompatEditText;
 
 import com.hardskygames.luckycalories.ButterKnifeFragment;
 import com.hardskygames.luckycalories.R;
@@ -9,10 +10,21 @@ import com.hardskygames.luckycalories.launch.models.SignUp;
 
 import javax.inject.Inject;
 
+import butterknife.Bind;
+
 /**
  * A simple {@link Fragment} subclass.
  */
 public class SignUpFragment extends ButterKnifeFragment {
+
+    @Bind(R.id.txtName)
+    AppCompatEditText txtName;
+    @Bind(R.id.txtEmail)
+    AppCompatEditText txtEmail;
+    @Bind(R.id.txtPassword)
+    AppCompatEditText txtPassword;
+    @Bind(R.id.txtRepeatPassword)
+    AppCompatEditText txtRepeatPassword;
 
     @Inject
     SignUp model;
