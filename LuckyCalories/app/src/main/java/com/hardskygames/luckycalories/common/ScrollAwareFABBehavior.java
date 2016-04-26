3e7651ad46fc20cf52b24bdfd69b13ceac1c9c21
hardsky <hardsky@yandex.ru>
2016-04-26 15:15:51 +0300

    add FAB

---
+++ LuckyCalories/app/src/main/java/com/hardskygames/luckycalories/common/ScrollAwareFABBehavior.java
@@ -0,0 +1,40 @@
+package com.hardskygames.luckycalories.common;
+
+import android.content.Context;
+import android.support.design.widget.CoordinatorLayout;
+import android.support.design.widget.FloatingActionButton;
+import android.support.v4.view.ViewCompat;
+import android.util.AttributeSet;
+import android.view.View;
+
+/**
+ * Created by Nikolay Mihailov <hardsky@yandex.ru>  on 26.04.16.
+ * see https://github.com/codepath/android_guides/wiki/Floating-Action-Buttons
+ */
+public class ScrollAwareFABBehavior extends FloatingActionButton.Behavior {
+
+    public ScrollAwareFABBehavior(Context context, AttributeSet attrs) {
+        super();
+    }
+
+    @Override
+    public boolean onStartNestedScroll(CoordinatorLayout coordinatorLayout,
+                                       FloatingActionButton child, View directTargetChild, View target, int nestedScrollAxes) {
+        return nestedScrollAxes == ViewCompat.SCROLL_AXIS_VERTICAL ||
+                super.onStartNestedScroll(coordinatorLayout, child, directTargetChild, target,
+                        nestedScrollAxes);
+    }
+
+    @Override
+    public void onNestedScroll(CoordinatorLayout coordinatorLayout, FloatingActionButton child,
+                               View target, int dxConsumed, int dyConsumed, int dxUnconsumed, int dyUnconsumed) {
+        super.onNestedScroll(coordinatorLayout, child, target, dxConsumed, dyConsumed, dxUnconsumed,
+                dyUnconsumed);
+
+        if (dyConsumed > 0 && child.getVisibility() == View.VISIBLE) {
+            child.hide();
+        } else if (dyConsumed < 0 && child.getVisibility() != View.VISIBLE) {
+            child.show();
+        }
+    }
+}
