60dedb84ef606579f0b51b94d38e4d0cdb2e54c9
hardsky <hardsky@yandex.ru>
2016-04-28 09:24:06 +0300

    minor changes

--- LuckyCalories/app/src/androidTest/java/com/hardskygames/luckycalories/CaloriesTest.java
+++ LuckyCalories/app/src/androidTest/java/com/hardskygames/luckycalories/CaloriesTest.java
@@ -1,27 +1,20 @@
 package com.hardskygames.luckycalories;
 
-import android.support.test.espresso.UiController;
-import android.support.test.espresso.ViewAction;
 import android.support.test.espresso.contrib.RecyclerViewActions;
 import android.support.test.filters.LargeTest;
 import android.support.test.rule.ActivityTestRule;
 import android.support.test.runner.AndroidJUnit4;
-import android.view.View;
 
 import com.hardskygames.luckycalories.list.CaloriesActivity;
 
-import org.hamcrest.Matcher;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-
-import java.util.concurrent.atomic.AtomicReference;
 
 import static android.support.test.espresso.Espresso.onView;
 import static android.support.test.espresso.action.ViewActions.swipeLeft;
 import static android.support.test.espresso.assertion.ViewAssertions.matches;
 import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
-import static android.support.test.espresso.matcher.ViewMatchers.withHint;
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
 import static android.support.test.espresso.matcher.ViewMatchers.withText;
 import static org.hamcrest.Matchers.not;
