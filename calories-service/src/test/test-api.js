5391e30fbbbbadcf8731024d772788e3e975d294
hardsky <hardsky@yandex.ru>
2016-05-02 18:13:06 +0300

    minor changes

--- calories-service/src/test/test-api.js
+++ calories-service/src/test/test-api.js
@@ -25,7 +25,21 @@ describe('User Calories', function() {
                 done();
             });
     });
-    it('should add a SINGLE user on /user/<id>/calories POST');
+    it('should add a SINGLE user on /user/<id>/calories POST', function(done) {
+        chai.request('http://localhost:4003')
+            .post('/api/v1/user/1/calories')
+            .send({
+                "id": 0,
+                "meal": "test2",
+                "note": "test",
+                "amount": 100,
+                "eatTime": 1462171592000
+            })
+            .end(function(err, res){
+                res.should.have.status(200);
+                done();
+            });
+    });
     it('should update a SINGLE user on /user/<id>/calories PUT');
 });
 
